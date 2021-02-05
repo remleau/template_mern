a3141cbf71c198cbb1242366fed369c5feec2dc5
Remleau <remleau@gmail.com>
2021-02-05 00:24:45 -0500

    design header admin

--- client/src/mernpark-admin/components/header/Header.js
+++ client/src/mernpark-admin/components/header/Header.js
@@ -1,9 +1,58 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
+import styles from '../../assets/styles/main.module.css';
+
+import ProfilCard from '../profilCard';
 
 const Header = () => {
   return (
     <header>
-      header
+      <div className={styles.textMenu}>
+        <h2 className={styles.h2}>MernPark</h2>
+      </div>
+      <nav className={styles.menu}>
+        <ul className={styles.subMenu}>
+
+          <NavLink exact to="/admin?" activeClassName={styles.actif}>
+            <li className={styles.menuItem}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+              </svg>
+              <span>
+                Dashboard
+              </span>
+            </li>
+          </NavLink>
+
+          <NavLink to="/admin/settings" activeClassName={styles.actif}>
+            <li className={styles.menuItem}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+              </svg>
+              <span>
+                Settings    
+              </span>
+            </li>
+          </NavLink>
+
+          <NavLink to="/admin/features" activeClassName={styles.actif}>
+            <li className={styles.menuItem}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+              </svg>
+              <span>
+                Features
+              </span>
+            </li>
+          </NavLink>
+
+        </ul>
+      </nav>
+
+      <div className={styles.menuAccount}>
+        <ProfilCard />
+      </div>
     </header>
   );
 }
